f0549a977e015417c4eaed5e24faef4e0848cab5
Thibault James <thibault.james1@gmail.com>
2024-01-10 21:06:07 +0100

    Correction pour merge avec mes fichiers

--- Loans_simulator/app.R
+++ Loans_simulator/app.R
@@ -1,7 +1,80 @@
 library(shiny)
 library(shinydashboard)
+
 library(dplyr)
 source("functions.R")
+
+
+calcule_mensualite <- function(montant_emprunte, taux_annuel, duree_mois) {
+  
+  #' Calcule le montant d'une mensualité en fonction du montant à emprunter, du taux d’intérêt et de la durée du prêt.
+  #'
+  #' @param montant_emprunte Montant à emprunter.
+  #' @param taux_annuel Taux d'intérêt annuel en pourcentage.
+  #' @param duree_mois Durée du prêt en mois.
+  #'
+  #' @return Le montant de la mensualité.
+  #'
+  #' @examples
+  #' calcule_mensualite(10000, 5, 36)
+
+  taux_mensuel <- taux_annuel / 12 / 100  # Calcul du taux d'intérêt mensuel en pourcentage
+  
+  # Calcul de la mensualité en utilisant la formule de calcul des mensualités d'un prêt
+  mensualite <- (montant_emprunte * taux_mensuel) / (1 - (1 + taux_mensuel)^-duree_mois)
+  montant_emp
+  
+  mensualite
+}
+
+
+
+tableau_amortissement <- function(montant_emprunte, taux_annuel, duree_mois, taux_assurance) {
+  
+  #' Génère un tableau d'amortissement pour un prêt en fonction du montant emprunté, du taux d'intérêt, de la durée du prêt et du montant de l'assurance.
+  #'
+  #' @param montant_emprunte Montant emprunté.
+  #' @param taux_annuel Taux d'intérêt annuel en pourcentage.
+  #' @param duree_mois Durée du prêt en mois.
+  #' @param assurance taux de l'assurance mensuelle.
+  #'
+  #' @return Un tableau décrivant l'amortissement du prêt mois par mois.
+  #'
+  #' @examples
+  #' tableau_amortissement(10000, 5, 36, 20)
+  
+  mensualite <- calcule_mensualite(montant_emprunte, taux_annuel, duree_mois)  # Calcul de la mensualité
+  mensualite <- round(mensualite,2)
+  capital_restant <- montant_emprunte
+  amortissement <- data.frame(Numéro = numeric(),
+                              Intérêts = numeric(),
+                              Principal = numeric(),
+                              Assurance = numeric(),
+                              Mensualité = numeric(),
+                              `Capital restant dû` = numeric(),
+                              stringsAsFactors = FALSE)
+  
+  # Calcul du montant de l'assurance 
+  
+  assurance <- taux_assurance * duree_mois/12 * montant_emprunte
+  taux_mensuel <- taux_annuel /12 /100
+  
+  
+  # Ajouter du code pour la dernière mensualité 
+  
+  for (mois in 1:duree_mois) {
+    interets <- capital_restant * taux_mensuel  # Calcul des intérêts pour ce mois
+    principal <- mensualite - interets - assurance  # Calcul du montant du principal pour ce mois
+    capital_restant <- capital_restant - principal  # Calcul du capital restant après paiement du principal
+    
+    ligne <- c(mois, interets, principal, assurance, mensualite, capital_restant)
+    amortissement <- rbind(amortissement, ligne)
+  }
+  
+  colnames(amortissement) <- c("Numéro", "Intérêts", "Principal", "Assurance", "Mensualité", "Capital restant dû")
+  amortissement
+}
+
 
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
